ea791dccf6b0db26f9c44cd787d08ce16a4f0079
Eva <okamenoame@gmail.com>
2023-03-28 21:48:38 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num % 10 > 0 )
+{
+    int digit = num / 10;
+    int secondDigit = digit % 10;
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
+
