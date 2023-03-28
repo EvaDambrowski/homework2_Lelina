cfd18a1b81c65d75fece80dfdc240a2d1c2bcbf8
Eva <okamenoame@gmail.com>
2023-03-28 22:51:04 +0300

    add task 13

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,15 +1,13 @@
-﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.WriteLine("Введите трехзначное число: ");
+﻿Console.WriteLine("Введите трехзначное число: ");
 int num = int.Parse(Console.ReadLine()!);
 
-if (num % 10 > 0 )
+if (num  / 100 > 0 )
 {
-    int digit = num / 10;
-    int secondDigit = digit % 10;
+   int digit = num / 10;
+   int secondDigit = digit % 10;
     Console.WriteLine(secondDigit);
 }
 else
 {
     Console.WriteLine("Введено не трехзначное число");
 }
-
--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,17 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num  / 100 > 0 )
+{
+   //int digit = num / 10;
+   int digit = num % 10;
+    Console.WriteLine(digit);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
